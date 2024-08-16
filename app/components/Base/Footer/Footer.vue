9075fd5129953c4f3f1076c3f67948e6069160c1
Billy Maulana <billymaulana1999@gmail.com>
2024-08-17 03:24:16 +0700

    feat: update footer styles

--- app/components/Base/Footer/Footer.vue
+++ app/components/Base/Footer/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <footer class="footer">Copyright &copy;2024 Billy Maulana.</footer>
+  <footer class="footer">
+    Copyright &copy;2024 Billy Maulana.
+  </footer>
 </template>
 
 <style scoped lang="css">
@@ -8,6 +10,7 @@
   width: 100%;
   padding: 10px;
   text-align: center;
-  font-size: 12px;
+  font-size: 14px;
+  border-top: 1px solid #f4f5f9;
 }
 </style>
