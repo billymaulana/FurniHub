d9af3804b62a430fc0a6cb081bb67cc80898d16b
Billy Maulana <billymaulana1999@gmail.com>
2024-08-18 12:31:12 +0700

    feat: enhance ui

--- app/app.vue
+++ app/app.vue
@@ -1,19 +1,21 @@
 <script setup lang="ts">
-import { appName } from "~/constants";
+import { appName } from '~/constants'
 
 useHead({
   title: appName,
-  titleTemplate: (title) => (title !== appName ? `${appName} | ${title}` : title),
+  titleTemplate: title => (title !== appName ? `${appName} | ${title}` : title),
   htmlAttrs: {
-    lang: "en",
+    lang: 'en',
   },
-});
+})
 </script>
 
 <template>
   <VitePwaManifest />
   <NuxtLayout>
-    <NuxtLoadingIndicator />
+    <NuxtLoadingIndicator
+      style="background: repeating-linear-gradient(to right, rgb(45 128 126) 0%, rgb(122 126 127) 50%, rgb(45 128 126) 100%);"
+    />
     <NuxtPage />
   </NuxtLayout>
 </template>
